57eeb468fa7c6be589c3313a4bc5ee9a032b5e08
tranzilla <huynhht@colorado.edu>
2019-06-18 13:10:06 -0700

    django blog post
    initial commit

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Create your views here - Logic of the application - Controller
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
